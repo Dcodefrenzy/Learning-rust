d5e552e4c4809b9b4a989002cc2571faff99dd58
Ayodeji Fakunle <34624631+Dcodefrenzy@users.noreply.github.com>
2022-02-22 18:33:18 +0000

    Added Trait to my struct

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,27 @@
-#[derive(Debug)]
+
 struct Person {
   name: String, 
   age:u32,
   gender:char,
-  phoneNumber:u64
 }
 
+trait CallPerson {
+  fn callName(&self)
+}
+
+impl CallPerson for Person {
+  fn callName(&self) {
+    println!("{}" &self.name);
+  }
+}
+  
 fn main() {
   let papa = Person {
     name: "Ayodeji Fakunle".to_string(), 
     age:25,
     gender:'M',
-    phoneNumber: 09133475878
     
   };
 
-  let Person {
-    name,
-    age,
-    gender,
-    phoneNumber
-  } = papa;
 
-println!("{}, age {}, gender {}", name, age, gender, phoneNumber);
 }
